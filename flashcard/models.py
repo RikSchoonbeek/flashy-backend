dc080c4d30351ded8346e5cddc548c5190ebb69a
RikSchoonbeek <rik.schoonbeek@gmail.com>
2018-06-30 13:05:48 +0200

    DRF set-up, one app: 'flashcard' with new models, just migrated to DB

---
+++ flashcard/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+
+class FlashCard(models.Model):
+    code = models.TextField()
+    problem = models.CharField(max_length=511)
+    topic = models.ForeignKey('Topic',
+                              on_delete=models.CASCADE,
+                              blank=True,
+                              null=True,
+                              )
+    language = models.ForeignKey('ProgrammingLanguage',
+                                 on_delete=models.CASCADE,
+                                 blank=True,
+                                 null=True,
+                                 )
+    source = models.CharField(max_length=511,
+                              blank=True,
+                              null=True,
+                              )
+
+class Topic(models.Model): #(relates to a user in a many to many relationship)
+    name = models.CharField(max_length=255)
+    # user = models.ForeignKey() # many to many
+
+class ProgrammingLanguage(models.Model):
+    name = models.CharField(max_length=127)
+    version = models.CharField(max_length=127,
+                               blank=True,
+                               null=True,
+                               )
+
+class Selection(models.Model): #(related to a specific card) - each selection on  a card get's it's own entry - index from + index to
+    index_from = models.PositiveIntegerField()
+    index_to = models.PositiveIntegerField()
+    flashcard = models.ForeignKey(FlashCard, on_delete=models.CASCADE)
