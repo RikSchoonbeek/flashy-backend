18993fc94799fe1eec99870f642ded1429e44093
RikSchoonbeek <rik.schoonbeek@gmail.com>
2018-06-30 16:20:51 +0200

    Registered Flashcard models on admin

--- flashcard/admin.py
+++ flashcard/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from flashcard import models
+
+
+@admin.register(models.FlashCard)
+class FlashCardAdmin(admin.ModelAdmin):
+    list_display = ('problem', 'topic', 'language')
+
+
+@admin.register(models.Topic)
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ('name', )
+
+
+@admin.register(models.ProgrammingLanguage)
+class ProgrammingLanguageAdmin(admin.ModelAdmin):
+    list_display = ('name', 'version', )
+
+
+@admin.register(models.Selection)
+class SelectionAdmin(admin.ModelAdmin):
+    list_display = ('flashcard', 'index_from', 'index_to')
