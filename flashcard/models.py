e017e8986ec656bde0634475c1c57b9976e66c90
RikSchoonbeek <rik.schoonbeek@gmail.com>
2018-06-30 16:21:45 +0200

    Added __str__ methods to models

--- flashcard/models.py
+++ flashcard/models.py
@@ -19,9 +19,17 @@ class FlashCard(models.Model):
                               null=True,
                               )
 
+    def __str__(self):
+        return self.problem
+
+
 class Topic(models.Model): #(relates to a user in a many to many relationship)
     name = models.CharField(max_length=255)
     # user = models.ForeignKey() # many to many
+
+    def __str__(self):
+        return self.name
+
 
 class ProgrammingLanguage(models.Model):
     name = models.CharField(max_length=127)
@@ -30,7 +38,17 @@ class ProgrammingLanguage(models.Model):
                                null=True,
                                )
 
+    def __str__(self):
+        name = self.name
+        if self.version:
+            name += (' ' + self.version)
+        return name
+
+
 class Selection(models.Model): #(related to a specific card) - each selection on  a card get's it's own entry - index from + index to
     index_from = models.PositiveIntegerField()
     index_to = models.PositiveIntegerField()
     flashcard = models.ForeignKey(FlashCard, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.flashcard.problem}: {self.index_from} - {self.index_to}"
